5aad852b8e84dfd9cada38d5e622710cac357461
masuhi <99360684+masuhi@users.noreply.github.com>
2022-07-23 22:56:08 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,4 +1,4 @@
-ORT_H
+#ifndef SORT_H
 #define SORT_H
 
 #include <stdio.h>
@@ -8,17 +8,17 @@ ORT_H
 
 /*Data structures*/
 /**
- *  * struct listint_s - Doubly linked list node
- *   *
- *    * @n: Integer stored in the node
- *     * @prev: Pointer to the previous element of the list
- *      * @next: Pointer to the next element of the list
- *       */
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 
